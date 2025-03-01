bc805079ea436f76e39189463ad0478c93e8a6b8
André Skrepchuk <andre.skcaldeira@gmail.com>
2025-03-01 19:55:56 +0000

    Using Data Annotations and Custom Validation Attributes

--- APICatalogo/Domain/Produto.cs
+++ APICatalogo/Domain/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -11,16 +12,19 @@ namespace APICatalogo.Domain
         public int Id { get; set; }
         [Required]
         [MaxLength(80)]
+        [FirstLetterUppercase]
         public string? Nome { get; set; }
         [Required]
         [MaxLength(300)]
         public string? Descricao { get; set; }
         [Required]
         [Column(TypeName="decimal(10,2)")]
+        [Range(1,10000, ErrorMessage = "O preço deve estar enter {1} e {2}")]
         public decimal Preco { get; set; }
         public float Estoque { get; set; }
         [Required]
         [MaxLength(300)]
+        [StringLength(300, MinimumLength = 20, ErrorMessage = "O URL deve conter caracteres com tamanho entre {2} e {1}")]
         public string? ImageUrl { get; set; }
         public DateTime DataCadastro { get; set; }
         public int CategoriaId { get; set; }
